514496ac7d2b7928587038e1639a9b4675431df1
Vansh <jain.vansh2102@gmail.com>
2025-08-24 18:14:02 +0530

    Create vansh.js
    completed the task of day 1

---
+++ Day1/vansh.js
@@ -0,0 +1,14 @@
+// Day 1 mini task- print student details from an object 
+
+const student = {
+    name: "Vansh Jain" ,
+    age: 19,
+    course: "B.tech CSE" ,
+    marks: 85
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("age:", student.age);
+console.log("course:", student.course);
+console.log("marks:", student.marks);
